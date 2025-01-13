75409985afcde5bbd1dc6b6d87e2733267263e10
Marie COLLET <marie.collet2023@gmail.com>
2025-01-13 14:57:12 +0100

    Mise en place des fichiers pour création des pages

---
+++ frontend/src/_utils/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = () => {
+    return (
+        <div>
+            error 404 A MODIFIER 
+        </div>
+    );
+};
+
+export default Error;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            Page accueil            
+        </div>
+    );
+};
+
+export default Home;
