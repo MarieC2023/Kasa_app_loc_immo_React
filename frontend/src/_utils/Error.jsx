927ccf129752e13ac6d89e99f8f1c40512480b04
Marie COLLET <marie.collet2023@gmail.com>
2025-01-20 09:33:51 +0100

    Correction route header + footer / Création de la page 404

--- frontend/src/_utils/Error.jsx
+++ frontend/src/_utils/Error.jsx
@@ -1,10 +1,19 @@
-
+import { Link } from "react-router-dom";
+import "./error.css"
 
 const Error = () => {
     return (
-        <div>
-            error 404 A MODIFIER 
-        </div>
+        <div className="error">
+        <p className="error_404">
+            404
+        </p>
+        <p className="error_text">
+            Oups! La page que vous demandez n&apos;existe pas.
+        </p>
+        <Link to="/home">
+            Retourner sur la page d&apos;accueil
+        </Link>
+    </div>
     );
 };
 
